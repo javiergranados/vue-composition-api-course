f88b425a3645f6941189e4bf30d4ef3f558a9528
Javier Granados <hola@javiergranados.dev>
2024-05-20 20:59:02 +0200

    Use useRoute and useRouter composables

--- src/views/PostDetails.vue
+++ src/views/PostDetails.vue
@@ -1,7 +1,33 @@
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+
+const goHome = () => {
+  setTimeout(() => {
+    router.push({ name: 'home' })
+  }, 3000)
+}
+
+const goToPost3 = () => {
+  router.push({ name: 'post', params: { id: 3 } })
+}
+
+const showPostId = () => {
+  alert('The post ID is ' + route.params.id)
+}
+</script>
+
 <template>
   <div class="post-details">
     <h1>Post Details</h1>
     <p>Display the details of a post with the ID {{ $route.params.id }}</p>
+    <div class="buttons">
+      <button @click="goHome">Go Home</button>
+      <button @click="goToPost3">Go to Post 3</button>
+      <button @click="showPostId">Show Post ID</button>
+    </div>
     <div class="backWrapper">
       <RouterLink class="back" to="/post-list">&lt; Back</RouterLink>
     </div>
@@ -10,13 +36,19 @@
 
 <style scoped>
 .post-details {
+  text-align: center;
   padding: 20px;
 }
-h1 {
-  text-align: center;
+.buttons {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+.buttons button {
+  margin: 0 10px;
 }
 .backWrapper {
   margin-top: 15px;
-  margin-bottom: 95px;
+  margin-bottom: 55px;
 }
 </style>
