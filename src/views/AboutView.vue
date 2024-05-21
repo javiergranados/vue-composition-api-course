0b7fd8099f33e9e9e748164ba8fcceb691dd0053
Javier Granados <hola@javiergranados.dev>
2024-05-21 12:23:28 +0200

    Use a ref to add a class name

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,17 @@
 <script setup>
+import { onMounted, ref } from 'vue'
 import { vAutofocus } from '../directives'
+
+const titleRef = ref(null)
+
+onMounted(() => {
+  titleRef.value.classList.add('title')
+})
 </script>
 
 <template>
   <div class="about">
-    <h1>About</h1>
+    <h1 ref="titleRef">About</h1>
     <textarea v-autofocus></textarea>
   </div>
 </template>
@@ -13,6 +20,9 @@ import { vAutofocus } from '../directives'
 .about {
   text-align: center;
   padding: 20px;
+}
+.title {
+  color: hsla(160, 100%, 37%, 1);
 }
 textarea {
   height: 80px;
