717df0579038b74d50b29fd6b90436d44de7a99c
Javier Granados <hola@javiergranados.dev>
2024-07-11 14:46:03 +0200

    Implement a modal component using slots and props

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,37 @@
+<script setup>
+defineProps({
+  showModal: Boolean
+})
+</script>
+
+<template>
+  <div class="modal" v-if="showModal">
+    <slot name="title">Modal Title</slot>
+    <slot></slot>
+    <button @click="$emit('closeModal')">Close</button>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  background-color: beige;
+  padding: 10px;
+  position: fixed;
+  top: 20%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+  max-width: 400px;
+  max-height: 300px;
+}
+.modal h1,
+.modal p {
+  text-align: left;
+}
+.modal button {
+  margin-top: 80px;
+}
+</style>
--- src/views/ModalView.vue
+++ src/views/ModalView.vue
@@ -1,7 +1,14 @@
 <script setup>
+import ModalComponent from '@/components/ModalComponent.vue'
 import { ref } from 'vue'
 
 const showModal = ref(false)
+
+defineEmits(['closeModal'])
+
+function closeModal() {
+  showModal.value = false
+}
 </script>
 
 <template>
@@ -9,15 +16,14 @@ const showModal = ref(false)
     <h1>Modal</h1>
     <button class="openButton" @click="showModal = true">Open Modal</button>
     <Teleport to="body">
-      <div class="modal" v-if="showModal">
-        <h1>Modal Title</h1>
+      <ModalComponent :show-modal="showModal" @closeModal="closeModal">
+        <template #title>New Modal Title</template>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptates beatae eveniet
-          doloremque placeat sint corrupti a, adipisci cupiditate praesentium. Animi aliquid illum
-          adipisci dolorem recusandae doloremque, atque maiores tenetur.
+          New body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptates beatae
+          eveniet doloremque placeat sint corrupti a, adipisci cupiditate praesentium. Animi aliquid
+          illum adipisci dolorem recusandae doloremque, atque maiores tenetur.
         </p>
-        <button @click="showModal = false">Close</button>
-      </div>
+      </ModalComponent>
     </Teleport>
   </div>
 </template>
@@ -29,26 +35,5 @@ const showModal = ref(false)
 }
 .openButton {
   margin-bottom: 135px;
-}
-.modal {
-  background-color: beige;
-  padding: 10px;
-  position: fixed;
-  top: 20%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  width: 100%;
-  height: 100%;
-  max-width: 400px;
-  max-height: 300px;
-}
-.modal h1,
-.modal p {
-  text-align: left;
-}
-.modal button {
-  margin-top: 80px;
 }
 </style>
