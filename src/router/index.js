7d0f157b38839fa83351e0328306bc4f7205bf7e
Javier Granados <hola@javiergranados.dev>
2024-07-11 14:12:46 +0200

    Remove lazy loading from the router file

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
 import PostListView from '../views/PostListView.vue'
 import ModalView from '../views/ModalView.vue'
+import PostDetails from '../views/PostDetails.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,10 +16,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutView
     },
     {
       path: '/post-list',
@@ -32,7 +31,7 @@ const router = createRouter({
     {
       path: '/post-list/:id',
       name: 'post',
-      component: () => import('../views/PostDetails.vue')
+      component: PostDetails
     }
   ]
 })
