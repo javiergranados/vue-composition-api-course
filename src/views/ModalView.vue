55e284f9533204edb3e5467e129988a8181daaf2
Javier Granados <hola@javiergranados.dev>
2024-07-11 15:04:03 +0200

    Implement dynamic components in the modal view

--- src/views/ModalView.vue
+++ src/views/ModalView.vue
@@ -1,25 +1,40 @@
 <template>
   <div class="modalView">
     <h1>Modal</h1>
+    <div>
+      <input
+        type="checkbox"
+        v-model="useDarkModal"
+        @click="useDarkModal = !useDarkModal"
+        id="useDarkModal"
+      />
+      <label for="useDarkModal" class="checkboxLabel">Use Dark Modal</label>
+    </div>
     <button class="openButton" @click="showModal = true">Open Modal</button>
     <Teleport to="body">
-      <modal-component :show-modal="showModal" @closeModal="closeModal">
+      <component
+        :is="useDarkModal ? DarkModal : LightModal"
+        :show-modal="showModal"
+        @closeModal="closeModal"
+      >
         <template #title>New Modal Title</template>
         <p>
           New body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptates beatae
           eveniet doloremque placeat sint corrupti a, adipisci cupiditate praesentium. Animi aliquid
           illum adipisci dolorem recusandae doloremque, atque maiores tenetur.
         </p>
-      </modal-component>
+      </component>
     </Teleport>
   </div>
 </template>
 
 <script setup>
-import ModalComponent from '@/components/ModalComponent.vue'
+import LightModal from '@/components/LightModal.vue'
+import DarkModal from '@/components/DarkModal.vue'
 import { ref } from 'vue'
 
 const showModal = ref(false)
+const useDarkModal = ref(false)
 
 defineEmits(['closeModal'])
 
@@ -30,10 +45,16 @@ function closeModal() {
 
 <style scoped>
 .modalView {
+  display: 'flex';
+  flex-direction: 'column';
   text-align: center;
   padding: 20px;
 }
+.checkboxLabel {
+  margin-left: 10px;
+}
 .openButton {
+  margin-top: 20px;
   margin-bottom: 135px;
 }
 </style>
