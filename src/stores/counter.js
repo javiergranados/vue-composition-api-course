ef84fc454e1be491db5dd7169f92aecd16423924
Javier Granados <hola@javiergranados.dev>
2024-07-11 19:15:59 +0200

    Write the pinia store with composition api

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,12 +1,17 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
 export const useCounterStore = defineStore('counter', () => {
+  const title = ref('')
   const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
+
   function increment() {
     count.value++
   }
 
-  return { count, doubleCount, increment }
+  function decrement() {
+    count.value--
+  }
+
+  return { title, count, increment, decrement }
 })
