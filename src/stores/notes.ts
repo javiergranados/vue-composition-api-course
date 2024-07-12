af81bc15eec58af44bafd7a385533fbf92a09039
Javier Granados <hola@javiergranados.dev>
2024-07-12 18:35:37 +0200

    Keep default values on the note store

--- src/stores/notes.ts
+++ src/stores/notes.ts
@@ -3,7 +3,17 @@ import { ref } from 'vue'
 import { Note } from '@/lib/definitions'
 
 export const useNoteStore = defineStore('notes', () => {
-  const notes = ref<Note[]>([])
+  const notes = ref<Note[]>([
+    {
+      id: 'id1',
+      content:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime.',
+    },
+    {
+      id: 'id2',
+      content: 'This is a shorter note! Woo!',
+    },
+  ])
 
   function addNote(note: Note) {
     notes.value.unshift(note)
