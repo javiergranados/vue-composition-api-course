e2e86a36e66d742fd5552382b6cbc4b28d1f2d57
Javier Granados <hola@javiergranados.dev>
2024-07-12 13:43:24 +0200

    Add Bulma to the project

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,15 @@
 <template>
   <nav>
-    <RouterLink to="/">Notes</RouterLink>
-    <RouterLink to="/stats">Stats</RouterLink>
+    <RouterLink
+      to="/"
+      class="button"
+      >Notes</RouterLink
+    >
+    <RouterLink
+      to="/stats"
+      class="button"
+      >Stats</RouterLink
+    >
   </nav>
   <RouterView />
 </template>
-
-<style scoped>
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-</style>
