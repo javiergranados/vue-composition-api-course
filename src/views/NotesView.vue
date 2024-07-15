35beebf16f07fedd3b43608bf7466eefbb91fecc
Javier Granados <hola@javiergranados.dev>
2024-07-15 12:09:24 +0200

    Implement the EditionNote component in the notes view

--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
 import { v4 as uuidv4 } from 'uuid'
 import { ref } from 'vue'
+import EditionNote from '@ui/EditionNote.vue'
 import Note from '@ui/Note.vue'
 import { useNoteStore } from '@/stores/notes'
 
 const noteStore = useNoteStore()
 
-const note = ref('')
-const noteRef = ref<HTMLInputElement | null>(null)
+const noteContent = ref('')
+const editionNoteRef = ref<typeof EditionNote | null>(null)
 
 const addNote = () => {
   noteStore.addNote({
     id: uuidv4(),
-    content: note.value,
+    content: noteContent.value,
   })
 
-  note.value = ''
-  noteRef.value?.focus()
+  noteContent.value = ''
+  editionNoteRef.value?.focusTextarea()
 }
 
 const deleteNote = (idToDelete: string) => {
@@ -25,29 +26,20 @@ const deleteNote = (idToDelete: string) => {
 </script>
 
 <template>
-  <div class="card has-background-success-on-scheme p-4">
-    <div class="field">
-      <div class="control">
-        <textarea
-          ref="noteRef"
-          v-model="note"
-          class="textarea"
-          placeholder="Write a new note..."
-        />
-      </div>
-    </div>
-    <div class="field is-grouped is-grouped-right">
-      <div class="control">
-        <button
-          :disabled="!note"
-          class="button is-link has-background-success"
-          @click.prevent="addNote"
-        >
-          Add
-        </button>
-      </div>
-    </div>
-  </div>
+  <EditionNote
+    ref="editionNoteRef"
+    v-model="noteContent"
+  >
+    <template #buttons>
+      <button
+        :disabled="!noteContent"
+        class="button is-link has-background-success"
+        @click.prevent="addNote"
+      >
+        Add
+      </button>
+    </template>
+  </EditionNote>
   <Note
     v-for="n in noteStore.notes"
     :key="n.id"
